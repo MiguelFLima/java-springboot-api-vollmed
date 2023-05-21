23d581e72967b05ac5b1669cba1d0e247967c6ef
Miguel <miguel2014ssa@gmail.com>
2023-05-21 14:53:45 -0300

    Pagination

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -6,6 +6,8 @@ import med.voll.api.paciente.DadosListagemPaciente;
 import med.voll.api.paciente.Paciente;
 import med.voll.api.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,8 +28,8 @@ public class PacienteController {
     }
 
     @GetMapping
-    public List<DadosListagemPaciente> getPacientes() {
-        return repository.findAll().stream().map(DadosListagemPaciente::new).toList();
+    public Page<DadosListagemPaciente> getPacientes(Pageable paginacao) {
+        return repository.findAll(paginacao).map(DadosListagemPaciente::new);
     }
 
 }
